9755de1724310b4cfaaa10eb5f77cd73a2b460fd
Anaïs Betts <anais@anaisbetts.org>
2022-11-25 05:37:43 +0100

    Upgrade the world and add dotenv

--- components/list-box.tsx
+++ components/list-box.tsx
@@ -1,6 +1,6 @@
 import { Fragment, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
-import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 
 import { cx } from '../lib/actions/utility'
 
@@ -37,7 +37,7 @@ export default function ListBox({ items, label, onItemSelect }: ListBoxProps) {
             <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm">
               <span className="block truncate">{selected?.summary}</span>
               <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                <SelectorIcon
+                <ChevronUpDownIcon
                   className="h-5 w-5 text-gray-400"
                   aria-hidden="true"
                 />
--- lib/api-helper.ts
+++ lib/api-helper.ts
@@ -3,6 +3,8 @@ import WebSocket from 'ws'
 const wnd: any = globalThis
 wnd.WebSocket = WebSocket
 
+require('dotenv').config()
+
 export const [homeAssistantUrl, homeAssistantToken] = [
   process.env.HA_BASE_URL!,
   process.env.HA_TOKEN!,
